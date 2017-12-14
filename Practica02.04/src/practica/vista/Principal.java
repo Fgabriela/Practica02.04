d8840d39069e3915f7d8ca427563d3a2ac3a1ebe
Estudiante <Estudiante@lab0510>
2017-12-14 09:39:30 -0500

    Creacion del proyecto

--- Practica02.04/src/practica/vista/Principal.java
+++ Practica02.04/src/practica/vista/Principal.java
@@ -1,10 +1,13 @@
 
 package practica.vista;
 
+import practica.controlador.GestionDato;
+
 public class Principal {
 
     public static void main(String[] args) {
         
-    }
-    
+        GestionDato gD = new GestionDato();
+            
+    }  
 }
